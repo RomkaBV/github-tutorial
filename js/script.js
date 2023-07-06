500a5057ca678999ffd852436b4fd6087b21b534
RomkaBV <barzo_romka@rambler.ru>
2023-07-06 15:31:09 +0300

    yes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("25");
+// Задача №1
+// const name = "Генератор захисного поля";
+// let price = 1000;
+// console.log("'Обрано «", name, "» ціна за штуку", price, "кредитів'");
+
+// price = 2000;
+// console.log("'Обрано «", name, "» ціна за штуку", price, "кредитів'");
+
+// Задача №2
+const total = 100;
+const ordered = 50;
